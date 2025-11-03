bdbb1af322ba064e28827e4da8a3b13af2a1d352
JY Lee <jylee.rahman@gmail.com>
2025-11-03 16:52:12 -0500

    add neon color

--- vite-express-project/src/client/App.tsx
+++ vite-express-project/src/client/App.tsx
@@ -9,10 +9,52 @@ function App() {
 
   return (
     <>
-    <div className="text-[100px] font-bold font-['Timegoing']">New York City</div>
-      <div className="text-[170px] font-bold font-['WhoopieSunday'] text-white">Photo Booth</div>
+    <div 
+      className="text-[100px] font-bold font-['Timegoing']"
+      style={{
+        color: '#FFD700',
+        textShadow: `
+          2px 2px 0 #000,
+          -2px -2px 0 #000,
+          2px -2px 0 #000,
+          -2px 2px 0 #000,
+          0 0 20px rgba(255, 215, 0, 0.5)
+        `
+      }}
+    >
+      New York City
+    </div>
+      <div 
+        className="text-[170px] font-bold font-['WhoopieSunday']"
+        style={{
+          color: '#FF1493',
+          textShadow: `
+            3px 3px 0 #000,
+            -3px -3px 0 #000,
+            3px -3px 0 #000,
+            -3px 3px 0 #000,
+            0 0 30px rgba(255, 20, 147, 0.6)
+          `
+        }}
+      >
+        Photo Booth
+      </div>
       <nav>
-        <div className="text-6xl font-bold font-['Timegoing']"><Link to="/frame">Start</Link></div>
+        <div 
+          className="text-6xl font-bold font-['Timegoing']"
+          style={{
+            color: '#00FFFF',
+            textShadow: `
+              2px 2px 0 #000,
+              -2px -2px 0 #000,
+              2px -2px 0 #000,
+              -2px 2px 0 #000,
+              0 0 20px rgba(0, 255, 255, 0.5)
+            `
+          }}
+        >
+          <Link to="/frame">Start</Link>
+        </div>
       </nav>
     </>
   );
